e2578828225f44e151e3e47f991b2cf2754d1113
dan.hughes <dan.hughes@heathwallace.com>
2015-09-28 14:35:19 +0100

    Send result rather than just logging it. Woops

--- app/responses/calculate.js
+++ app/responses/calculate.js
@@ -4,9 +4,9 @@ module.exports = function(user, request, respond) {
 
 	var expression = request.slice(request.indexOf('calculate') + 'calculate'.length);
 
-	console.log(expression + ' = ' + math.eval(expression));
+	var result = expression + ' = ' + math.eval(expression);
 
 	respond({
-		text : user + '345'
+		text : user + result
 	});
 };
