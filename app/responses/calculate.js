0a49b20dd7c0ee0a7e786217e56deda6f045e3d1
dan.hughes <dan.hughes@heathwallace.com>
2015-09-28 14:49:30 +0100

    Respond with an error if Calculate doesn't know what to do

--- app/responses/calculate.js
+++ app/responses/calculate.js
@@ -4,7 +4,13 @@ module.exports = function(user, request, respond) {
 
 	var expression = request.slice(request.indexOf('calculate') + 'calculate'.length);
 
-	var result = expression + ' = ' + math.eval(expression);
+	var result;
+
+	try {
+		result = expression + ' = ' + math.eval(expression);
+	} catch(err) {
+		result = 'I don\'t understand that expression!';
+	}
 
 	respond({
 		text : user + result
